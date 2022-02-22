e6b861fc757769583ecef66a5f48025cfb21e577
Leonardo Di Salvo <leonardodisalvo99@gmail.com>
2022-02-22 16:43:38 +0100

    added js sidebar

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function showMenu() {
     let menuOne = document.getElementById("blackbar-menu-one");
 
-    if(menuOne.style.visibility === "collapse") {
+    if (menuOne.style.visibility === "collapse") {
         menuOne.style.visibility = "visible";
     } else {
         menuOne.style.visibility = "collapse";
     }
 }
 
+function showside() {
+    let body = document.querySelector("body")
+    let homepage = document.getElementById("homepage")
+    let sidebar = document.getElementById("sidebar")
+    if (sidebar.style.visibility === "collapse") {
+        sidebar.style.visibility = "visible"
+        homepage.style.filter = "brightness(50%)"
+        body.style.overflowY = "hidden"
 
+    } else {
+        sidebar.style.visibility = "collapse"
+        homepage.style.filter = ""
+        homepage.style.overflowY = "hidden"
+        body.style.overflowY = "scroll"
+    }
+}
